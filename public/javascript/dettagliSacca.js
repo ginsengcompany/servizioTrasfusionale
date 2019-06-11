17a1b30ff516ed2a510b25cc0d33105886fa331b
AlRivoli <pleyboy87@hotmail.it>
2019-06-11 16:12:03 +0200

    finita parte grafica dettagli sacca

--- public/javascript/dettagliSacca.js
+++ public/javascript/dettagliSacca.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
+    let date = moment($('#dateStamp').val(), 'DD-MM-YYYY HH:mm ').toDate();
+    $('#dateStamp').datetimepicker({
+        date:date,
+        locale: 'it',
+        ignoreReadonly: true
+    });
+    let dateScadenza = moment($('#dataScadenza').val(), 'DD-MM-YYYY').toDate();
+    $('#dataScadenza').datetimepicker({
+        date:dateScadenza,
+        locale: 'it',
+        format: 'L',
+        ignoreReadonly: true
+    });
+    let dateSomministrazione = moment($('#dataSomministrazione').val(), 'DD-MM-YYYY HH:mm ').toDate();
+    $('#dataSomministrazione').datetimepicker({
+        date:dateSomministrazione,
+        locale: 'it',
+        ignoreReadonly: true
+    });
 
 });
